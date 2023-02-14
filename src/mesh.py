7da84f18a4d2428fc1b71406bce37051d551b758
Ali A Eftekhari <e.eftekhari@gmail.com>
2023-02-15 00:26:45 +0100

    mesh is almost finalized

--- src/mesh.py
+++ src/mesh.py
@@ -11,11 +11,13 @@ class CellSize:
         self.x = x
         self.y = y
         self.z = z
+
     def __str__(self):
         temp = vars(self)
         for item in temp:
             print(item, ':', temp[item])
         return ""
+
     def __repr__(self):
         temp = vars(self)
         for item in temp:
@@ -28,11 +30,13 @@ class CellLocation:
         self.x = x
         self.y = y
         self.z = z
+
     def __str__(self):
         temp = vars(self)
         for item in temp:
             print(item, ':', temp[item])
         return ""
+
     def __repr__(self):
         temp = vars(self)
         for item in temp:
@@ -45,16 +49,19 @@ class FaceLocation:
         self.x = x
         self.y = y
         self.z = z
+
     def __str__(self):
         temp = vars(self)
         for item in temp:
             print(item, ':', temp[item])
         return ""
+
     def __repr__(self):
         temp = vars(self)
         for item in temp:
             print(item, ':', temp[item])
         return ""
+
 
 class MeshStructure:
     def __init__(self, dimension, dims, cellsize,
@@ -109,16 +116,20 @@ class Mesh1D(MeshStructure):
                 1, Nx, Nx)*dx-dx/2, np.array([0.0]), np.array([0.0]))
             face_location = FaceLocation(np.linspace(
                 0, Nx, Nx)*dx, np.array([0.0]), np.array([0.0]))
-        
+
         self.dims = np.array([Nx], dtype=int)
         self.cellsize = cell_size
         self.cellcenters = cell_location
         self.facecenters = face_location
         self.corners = np.array([1], dtype=int)
         self.edges = np.array([1], dtype=int)
+    def cell_numbers(self):
+        Nx = self.dims[0]
+        return int_range(0, Nx+1)
     def __repr__(self):
         print(f"1D Cartesian mesh with {self.dims[0]} cells")
         return ""
+
 
 class Mesh2D(MeshStructure):
     def __init__(self, *args):
@@ -129,12 +140,12 @@ class Mesh2D(MeshStructure):
             facelocationY = args[1]
             Nx = facelocationX.size-1
             Ny = facelocationY.size-1
-            cell_size = CellSize(_facelocation_to_cellsize(facelocationX), 
-                                 _facelocation_to_cellsize(facelocationY), 
+            cell_size = CellSize(_facelocation_to_cellsize(facelocationX),
+                                 _facelocation_to_cellsize(facelocationY),
                                  np.array([0.0]))
             cell_location = CellLocation(
-                0.5*(facelocationX[1:]+facelocationX[0:-1]), 
-                0.5*(facelocationY[1:]+facelocationY[0:-1]), 
+                0.5*(facelocationX[1:]+facelocationX[0:-1]),
+                0.5*(facelocationY[1:]+facelocationY[0:-1]),
                 np.array([0.0]))
             face_location = FaceLocation(
                 facelocationX,
@@ -149,29 +160,34 @@ class Mesh2D(MeshStructure):
             dx = Width/Nx
             dy = Height/Ny
             cell_size = CellSize(
-                dx*np.ones(Nx+2), 
-                dy*np.ones(Ny+2), 
+                dx*np.ones(Nx+2),
+                dy*np.ones(Ny+2),
                 np.array([0.0]))
             cell_location = CellLocation(
-                np.linspace(1, Nx, Nx)*dx-dx/2, 
-                np.linspace(1, Ny, Ny)*dy-dy/2, 
+                np.linspace(1, Nx, Nx)*dx-dx/2,
+                np.linspace(1, Ny, Ny)*dy-dy/2,
                 np.array([0.0]))
             face_location = FaceLocation(
-                np.linspace(0, Nx, Nx)*dx, 
+                np.linspace(0, Nx, Nx)*dx,
                 np.linspace(0, Ny, Ny)*dy,
                 np.array([0.0]))
-        
+
         self.dims = np.array([Nx, Ny], dtype=int)
         self.cellsize = cell_size
         self.cellcenters = cell_location
         self.facecenters = face_location
         G = int_range(1, (Nx+2)*(Ny+2))-1
-        self.corners = G.reshape(Nx+2, Ny+2)[[0,-1,0,-1], [0, 0, -1, -1]]
+        self.corners = G.reshape(Nx+2, Ny+2)[[0, -1, 0, -1], [0, 0, -1, -1]]
         self.edges = np.array([1], dtype=int)
+    def cell_numbers(self):
+        Nx, Ny = self.dims
+        G = int_range(0, (Nx+2)*(Ny+2)-1)
+        return G.reshape(Nx+2, Ny+2)
     def __repr__(self):
         print(f"2D Cartesian mesh with {self.dims[0]}x{self.dims[1]} cells")
         return ""
-    
+
+
 class Mesh3D(MeshStructure):
     def __init__(self, *args):
         self.dimension = 2
@@ -183,12 +199,12 @@ class Mesh3D(MeshStructure):
             Nx = facelocationX.size-1
             Ny = facelocationY.size-1
             Nz = facelocationZ.size-1
-            cell_size = CellSize(_facelocation_to_cellsize(facelocationX), 
-                                 _facelocation_to_cellsize(facelocationY), 
+            cell_size = CellSize(_facelocation_to_cellsize(facelocationX),
+                                 _facelocation_to_cellsize(facelocationY),
                                  _facelocation_to_cellsize(facelocationZ))
             cell_location = CellLocation(
-                0.5*(facelocationX[1:]+facelocationX[0:-1]), 
-                0.5*(facelocationY[1:]+facelocationY[0:-1]), 
+                0.5*(facelocationX[1:]+facelocationX[0:-1]),
+                0.5*(facelocationY[1:]+facelocationY[0:-1]),
                 0.5*(facelocationZ[1:]+facelocationZ[0:-1]))
             face_location = FaceLocation(
                 facelocationX,
@@ -206,89 +222,113 @@ class Mesh3D(MeshStructure):
             dy = Height/Ny
             dz = Depth/Nz
             cell_size = CellSize(
-                dx*np.ones(Nx+2), 
-                dy*np.ones(Ny+2), 
+                dx*np.ones(Nx+2),
+                dy*np.ones(Ny+2),
                 dz*np.ones(Nz+2))
             cell_location = CellLocation(
-                np.linspace(1, Nx, Nx)*dx-dx/2, 
-                np.linspace(1, Ny, Ny)*dy-dy/2, 
+                np.linspace(1, Nx, Nx)*dx-dx/2,
+                np.linspace(1, Ny, Ny)*dy-dy/2,
                 np.linspace(1, Nz, Nz)*dz-dz/2)
             face_location = FaceLocation(
-                np.linspace(0, Nx, Nx)*dx, 
+                np.linspace(0, Nx, Nx)*dx,
                 np.linspace(0, Ny, Ny)*dy,
                 np.linspace(0, Nz, Nz)*dz)
-        
+        G = int_range(1, (Nx+2)*(Ny+2)*(Nz+2))-1
+        G = G.reshape(Nx+2, Ny+2, Nz+2)
         self.dims = np.array([Nx, Ny, Nz], dtype=int)
         self.cellsize = cell_size
         self.cellcenters = cell_location
         self.facecenters = face_location
-        self.corners = np.array([1], dtype=int)
-        self.edges = np.array([1], dtype=int)
+        self.corners = G[np.ix_((0, -1), (0, -1), (0, -1))].flatten()
+        self.edges = np.hstack([G[0, [0, -1], 1:-1].flatten(),
+                                G[-1, [0, -1], 1:-1].flatten(),
+                                G[0, 1:-1, [0, -1]].flatten(),
+                                G[-1, 1:-1, [0, -1]].flatten()])
+    
+    def cell_numbers(self):
+        Nx, Ny, Nz = self.dims
+        G = int_range(0, (Nx+2)*(Ny+2)*(Nz+2)-1)
+        return G.reshape(Nx+2, Ny+2, Nz+2)
+    
     def __repr__(self):
-        print(f"3D Cartesian mesh with Nx={self.dims[0]}xNy={self.dims[1]}xNz={self.dims[1]} cells")
+        print(
+            f"3D Cartesian mesh with Nx={self.dims[0]}xNy={self.dims[1]}xNz={self.dims[1]} cells")
         return ""
+
 
 class MeshCylindrical1D(Mesh1D):
     def __init__(self, *args):
         Mesh1D.__init__(self, *args)
         self.dimension = 1.5
+
     def __repr__(self):
         print(f"1D Cylindrical (radial) mesh with Nr={self.dims[0]} cells")
         return ""
+
 
 class MeshSpherical1D(Mesh1D):
     def __init__(self, *args):
         Mesh1D.__init__(self, *args)
         self.dimension = 1.8
+
     def __repr__(self):
         print(f"1D Spherical mesh with Nr={self.dims[0]} cells")
         return ""
+
 
 class MeshCylindrical2D(Mesh2D):
     def __init__(self, *args):
         Mesh2D.__init__(self, *args)
         self.dimension = 2.5
+
     def __repr__(self):
-        print(f"2D Cylindrical mesh with Nr={self.dims[0]}xNz={self.dims[1]} cells")
+        print(
+            f"2D Cylindrical mesh with Nr={self.dims[0]}xNz={self.dims[1]} cells")
         return ""
+
 
 class MeshRadial2D(Mesh2D):
     def __init__(self, *args):
-        if args[3]>np.pi:
+        if args[3] > np.pi:
             warn("Recreate the mesh with an upper bound of 2*pi for \theta or there will be unknown consequences!")
         Mesh2D.__init__(self, *args)
         self.dimension = 2.8
+
     def __repr__(self):
-        print(f"2D Radial mesh with Nr={self.dims[0]}xN_theta={self.dims[1]} cells")
+        print(
+            f"2D Radial mesh with Nr={self.dims[0]}xN_theta={self.dims[1]} cells")
         return ""
+
 
 class MeshCylindrical3D(Mesh3D):
     def __init__(self, *args):
-        if args[4]>np.pi:
+        if args[4] > np.pi:
             warn("Recreate the mesh with an upper bound of 2*pi for \theta or there will be unknown consequences!")
         Mesh3D.__init__(self, *args)
         self.dimension = 3.2
+
     def __repr__(self):
-        print(f"3D Cylindrical mesh with Nr={self.dims[0]}xN_theta={self.dims[1]}xNz={self.dims[1]} cells")
+        print(
+            f"3D Cylindrical mesh with Nr={self.dims[0]}xN_theta={self.dims[1]}xNz={self.dims[1]} cells")
         return ""
+
 
 class MeshSpherical3D(Mesh3D):
     def __init__(self, *args):
-        if args[4]>np.pi:
+        if args[4] > np.pi:
             warn("Recreate the mesh with an upper bound of 2*pi for \theta or there will be unknown consequences!")
-        if args[5]>np.pi:
+        if args[5] > np.pi:
             warn("Recreate the mesh with an upper bound of 2*pi for \phi or there will be unknown consequences!")
         Mesh3D.__init__(self, *args)
         self.dimension = 3.5
+
     def __repr__(self):
-        print(f"3D Shperical mesh with Nr={self.dims[0]}xN_theta={self.dims[1]}xN_phi={self.dims[1]} cells")
+        print(
+            f"3D Shperical mesh with Nr={self.dims[0]}xN_theta={self.dims[1]}xN_phi={self.dims[1]} cells")
         return ""
+
 
 def _facelocation_to_cellsize(facelocation):
     return np.hstack([facelocation[1]-facelocation[0],
-                                     facelocation[1:]-facelocation[0:-1],
-                                     facelocation[-1]-facelocation[-2]])
-
-
-
-
+                      facelocation[1:]-facelocation[0:-1],
+                      facelocation[-1]-facelocation[-2]])
--- src/test_dev.py
+++ src/test_dev.py
@@ -8,6 +8,9 @@ from boundary import *
 # Development story
 m1 = MeshCylindrical1D(10, 1.0)
 m2 = MeshCylindrical2D(10, 5, 1.0, 1.0)
+m3 = Mesh3D(4, 5, 6, 1.0, 2.0, 3.0)
+print(m3)
+print(m3.cell_numbers())
 # TBD: call all mesh functions
 # Could create some of the meshes
 
