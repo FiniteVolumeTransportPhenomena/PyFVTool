550b801ea08a95d841a30d32f580738dc401e881
Ali A Eftekhari <e.eftekhari@gmail.com>
2023-03-17 00:34:46 +0100

    more tests

--- tests/test_benchmark_1d.py
+++ tests/test_benchmark_1d.py
@@ -58,7 +58,41 @@ def T_numerical(left_bc: str) -> float:
     T_an = T_analytic(x, t_sim)
     er = np.sum(np.abs(T_num-T_an)/T_an)/Nx
     return er
-     
+
+def conv_numerical_1d() -> float:
+    L = 1.0  # domain length
+    Nx = 25 # number of cells
+    meshstruct = createMesh1D(Nx, L)
+    BC = createBC(meshstruct) # all Neumann boundary condition structure
+    BC.left.a[:] = 0 
+    BC.left.b[:] = 1 
+    BC.left.c[:] = 0 # left boundary
+    BC.right.a[:] = 0 
+    BC.right.b[:] = 1 
+    BC.right.c[:] = 1 # right boundary
+    x = meshstruct.cellcenters.x
+    ## define the transfer coeffs
+    D_val = -1
+    D = createCellVariable(meshstruct, D_val)
+    Dave = harmonicMean(D) # convert a cell variable to face variable
+    # alfa = createCellVariable(meshstruct, 1)
+    u = -10
+    u_face = createFaceVariable(meshstruct, u)
+    ## solve
+    Mconv =  convectionTerm(u_face)
+    # Mconvupwind =  convectionUpwindTerm(u_face)
+    Mdiff = diffusionTerm(Dave)
+    [Mbc, RHSbc] = boundaryConditionTerm(BC)
+    M = Mconv-Mdiff-Mbc
+    # Mupwind = Mconvupwind-Mdiff-Mbc
+    RHS = -RHSbc
+    c = solvePDE(meshstruct, M, RHS)
+    # c_upwind = solvePDE(meshstruct, Mupwind, RHS)
+    c_analytical = (1-np.exp(u*x/D_val))/(1-np.exp(u*L/D_val))
+    er = np.sum(np.abs(c_analytical-c.value[1:Nx+1]))
+    return er
+
+
 class TestDiffusion(unittest.TestCase):
     def test_1d_dirichlet(self):
         print("\n Running 1D conduction heat transfer with Dirichlet boundary:")
@@ -72,3 +106,10 @@ class TestDiffusion(unittest.TestCase):
         er = T_numerical(left_bc)
         eps_T = 0.001
         self.assertLessEqual(er, eps_T)
+
+class TestConvection(unittest.TestCase):
+    def test_1d_convection(self):
+        print("\nRunning 1D convection:")
+        er = conv_numerical_1d()
+        eps_c = 0.001
+        self.assertLessEqual(er, eps_c)
