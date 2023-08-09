313fcb5db4209e65d89185b50bfc3295c9859f4f
Martinus Werts <31038793+mhvwerts@users.noreply.github.com>
2023-08-09 21:32:49 +0200

    Cosmetic changes to source.py
    - Changed the names of the arguments of the SourceTerm functions,  so that they are consistent with the general PDE definition from the documentation (README)
    - Tried (in a clumsy way) to further clarify the docstrings

--- pyfvtool/source.py
+++ pyfvtool/source.py
@@ -7,13 +7,16 @@ from .cell import *
 from .face import *
 
 
-def constantSourceTerm(phi0: CellVariable):
+def constantSourceTerm(gamma: CellVariable):
     """
-    Constant source term of a PDE.
-
+    Constant source term in a PDE.
+    
+    The value of this source `gamma` may be different in each cell, but is 
+    constant during the evolution of the PDE.
+    
     Parameters
     ----------
-    phi0: CellVariable
+    gamma: CellVariable
         Value of the source term
     
     Returns
@@ -25,41 +28,44 @@ def constantSourceTerm(phi0: CellVariable):
     --------
     >>> from pyfvtool import *
     >>> m = createMesh1D(10, 1.0)
-    >>> phi0 = createCellVariable(m, 1.0)
-    >>> RHS = constantSourceTerm(phi0)
+    >>> gamma = createCellVariable(m, 1.0)
+    >>> RHS = constantSourceTerm(gamma)
     """
-    m = phi0.domain
+    m = gamma.domain
     if issubclass(type(m), Mesh1D):
         Nx = m.dims[0]
         G = m.cell_numbers()
         row_index = G[1:Nx+1]  # main diagonal (only internal cells)
         RHS = np.zeros(Nx+2)
-        RHS[row_index] = phi0.value[1:-1]
+        RHS[row_index] = gamma.value[1:-1]
     elif issubclass(type(m), Mesh2D):
         Nx, Ny = m.dims
         G = m.cell_numbers()
         # main diagonal (only internal cells)
         row_index = G[1:Nx+1, 1:Ny+1].ravel()
         RHS = np.zeros((Nx+2)*(Ny+2))
-        RHS[row_index] = phi0.value[1:-1, 1:-1].ravel()
+        RHS[row_index] = gamma.value[1:-1, 1:-1].ravel()
     elif issubclass(type(m), Mesh3D):
         Nx, Ny, Nz = m.dims
         G = m.cell_numbers()
         # main diagonal (only internal cells)
         row_index = G[1:Nx+1, 1:Ny+1, 1:Nz+1].ravel()
         RHS = np.zeros((Nx+2)*(Ny+2)*(Nz+2))
-        RHS[row_index] = phi0.value[1:-1, 1:-1, 1:-1].ravel()
+        RHS[row_index] = gamma.value[1:-1, 1:-1, 1:-1].ravel()
     return RHS
 
 
-def linearSourceTerm(betta0: CellVariable):
+def linearSourceTerm(beta: CellVariable):
     """
-    Linear source term of a PDE.
+    Linear source term in a PDE.
+
+    The linear source term takes the form `beta*phi` where `phi` is the
+    solution variable and the factor `beta` a coefficient.
 
     Parameters
     ----------
-    betta0: CellVariable
-        Value of the source term
+    beta: CellVariable
+        Multiplicative coefficient for the solution variable
     
     Returns
     -------
@@ -70,21 +76,21 @@ def linearSourceTerm(betta0: CellVariable):
     --------
     >>> from pyfvtool import *
     >>> m = createMesh1D(10, 1.0)
-    >>> betta0 = createCellVariable(m, 1.0)
-    >>> RHS = linearSourceTerm(betta0)
+    >>> beta = createCellVariable(m, 1.0)
+    >>> RHS = linearSourceTerm(beta)
     """
-    m = betta0.domain
+    m = beta.domain
     if issubclass(type(m), Mesh1D):
         Nx = m.dims[0]
         G = m.cell_numbers()
-        AP_diag = betta0.value[1:-1]
+        AP_diag = beta.value[1:-1]
         row_index = G[1:Nx+1]  # main diagonal (only internal cells)
         return csr_array((AP_diag, (row_index, row_index)),
                          shape=((Nx+2), (Nx+2)))
     elif issubclass(type(m), Mesh2D):
         Nx, Ny = m.dims
         G = m.cell_numbers()
-        AP_diag = betta0.value[1:-1, 1:-1].ravel()
+        AP_diag = beta.value[1:-1, 1:-1].ravel()
         # main diagonal (only internal cells)
         row_index = G[1:Nx+1, 1:Ny+1].ravel()
         return csr_array((AP_diag, (row_index, row_index)),
@@ -92,7 +98,7 @@ def linearSourceTerm(betta0: CellVariable):
     elif issubclass(type(m), Mesh3D):
         Nx, Ny, Nz = m.dims
         G = m.cell_numbers()
-        AP_diag = betta0.value[1:-1, 1:-1, 1:-1].ravel()
+        AP_diag = beta.value[1:-1, 1:-1, 1:-1].ravel()
         # main diagonal (only internal cells)
         row_index = G[1:Nx+1, 1:Ny+1, 1:Nz+1].ravel()
         return csr_array((AP_diag, (row_index, row_index)),
@@ -131,3 +137,4 @@ def transientTerm(phi_old: CellVariable, dt, alfa):
     else:
         a = alfa
     return linearSourceTerm(a/dt), constantSourceTerm(a*phi_old/dt)
+
