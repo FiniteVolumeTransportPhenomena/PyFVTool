d11f6a8701bf1e4580b031debf539cb284110162
Martinus Werts <martinus.werts@ens-rennes.fr>
2023-08-17 22:40:11 +0200

    add a first, simple test for visualizeCells

--- fvtool_test.py
+++ fvtool_test.py
@@ -10,7 +10,7 @@ reload(pyfvtool)
 
 # Test script, run this before committing changes & creating pull requests...
 
-#TODO: Include visualizeCells testing
+#TODO: Include more visualizeCells testing
 #TODO: Futher organize this test script
 #TODO: Consider setting up pytest or other testing infrastructure
 
@@ -229,5 +229,62 @@ for t in np.arange(dt, final_t, dt):
 
 # analytical solution
 c_analytical = 1-erf(x/(2*np.sqrt(D_val*t)))
+plt.figure(1)
+plt.clf()
 plt.plot(x, c.internalCells(), x, c_analytical, 'r--')
+# plt.show()
+
+
+
+# Testing visualization...
+# (code from README.md )
+
+# Solving a 1D diffusion equation with a fixed concentration 
+# at the left boundary and a closed boundary on the right side
+Nx = 20 # number of finite volume cells
+Lx = 1.0 # [m] length of the domain 
+c_left = 1.0 # left boundary concentration
+c_init = 0.0 # initial concentration
+D_val = 1e-5 # diffusion coefficient (gas phase)
+t_simulation = 3600.0 # [s] simulation time
+dt = 60.0 # [s] time step
+
+m1 = createMesh1D(Nx, Lx) # mesh object
+bc = createBC(m1) # Neumann boundary condition by default
+
+# switch the left boundary to Dirichlet: fixed concentration
+bc.left.a[:] = 0.0
+bc.left.b[:] = 1.0
+bc.left.c[:] = c_left
+
+# create a cell variable with initial concentration
+c_old = createCellVariable(m1, c_init, bc)
+
+# assign diffusivity to cells
+D_cell = createCellVariable(m1, D_val)
+D_face = geometricMean(D_cell) # average value of diffusivity at the interfaces between cells
+
+# Discretization
+Mdiff = diffusionTerm(D_face)
+Mbc, RHSbc = boundaryConditionTerm(bc)
+
+# time loop
+t = 0
+while t<t_simulation:
+    t+=dt
+    Mt, RHSt = transientTerm(c_old, dt, 1.0)
+    c_new = solvePDE(m1, Mt-Mdiff+Mbc, RHSbc+RHSt)
+    c_old.update_value(c_new)
+
+plt.figure(2)
+plt.clf()
+visualizeCells(c_old)
+
+
+# Only use show() at the end of the script, since only a single call is needed
+# to display all open Figures.
+# However, `visualizeCells` also calls plt.show()... we need to figure out
+# if this is desirable.
+# In Notebooks and in Spyder, plt.show() is not necessary. It is only needed
+# when running scripts in a stand-alone Python interpreter.
 plt.show()
