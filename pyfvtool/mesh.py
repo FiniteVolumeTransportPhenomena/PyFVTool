6a5b42fc2f5e9bf49db063d3d931bc63353f0f1c
Martinus Werts <martinus.werts@univ-angers.fr>
2024-01-20 11:43:03 +0100

    fix some escape sequences (warnings)

--- pyfvtool/mesh.py
+++ pyfvtool/mesh.py
@@ -810,9 +810,12 @@ def createMeshSpherical3D(*args) -> MeshSpherical3D:
     The mesh is created in spherical coordinates.
     """
     if args[4] > 2*np.pi:
-        warn("Recreate the mesh with an upper bound of 2*pi for \theta or there will be unknown consequences!")
+        warn("Recreate the mesh with an upper bound of 2*pi for \\theta"\
+             " or there will be unknown consequences!")
     if args[5] > 2*np.pi:
-        warn("Recreate the mesh with an upper bound of 2*pi for \phi or there will be unknown consequences!")
-    dims, cellsize, cellcenters, facecenters, corners, edges = _mesh_3d_param(
-        *args)
-    return MeshSpherical3D(dims, cellsize, cellcenters, facecenters, corners, edges)
+        warn("Recreate the mesh with an upper bound of 2*pi for \\phi"\
+             " or there will be unknown consequences!")
+    dims, cellsize, cellcenters, facecenters, corners, edges =\
+        _mesh_3d_param(*args)
+    return MeshSpherical3D(dims, cellsize, cellcenters,
+                           facecenters, corners, edges)
